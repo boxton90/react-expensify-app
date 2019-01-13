f7c9f9c6f762ecc379f515839865bc710e996ab0
Daniel Fernandez Rabal <boxton.da@gmail.com>
2019-01-13 19:43:41 +0100

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// Object desctructuring
+
 // const person = {
 //     name: 'Daniel',
 //     age: 28,
@@ -15,14 +17,23 @@
 //     console.log(`It's ${temperature} in ${city}`)
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        // name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         // name: 'Penguin'
+//     }
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher
+// const { name: publisherName = 'Self-Published' } = book.publisher
+// console.log(publisherName)
 
-console.log(publisherName)
+// Array desctructuring
+
+// const address = ['Calle Lopera 1234', 'Madrid', 'Madrid', '13442']
+// const [, city, state = 'New York'] = address
+// console.log(`You are in ${city} ${state}.`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+const [itemName, , mediumPrice] = item
+console.log(`A medium ${itemName} cost ${mediumPrice}`)
