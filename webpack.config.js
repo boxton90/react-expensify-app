d72fb864452e3d28532f8c1b781f7068e65c7d20
Daniel Fernandez Rabal <boxton.da@gmail.com>
2019-01-30 21:38:05 +0100

    Regular vs Development Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = (env) => {
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'public', 'dist'),
+            contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
             publicPath: '/dist/'
         }
